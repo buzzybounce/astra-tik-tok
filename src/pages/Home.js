e4a1d10e4436c54afd1ca07f6e368f961c2d0120
root <jireh.liceralde@gmail.com>
2021-04-21 17:41:46 +0000

    Step 4 final

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+
+import '../App.css'
+const Home = () => {
+  return (
+    <div className="Home">
+    </div>
+  );
+}
+export default Home
---
+++ src/pages/Upload.js
@@ -0,0 +1,9 @@
+
+import '../App.css'
+const Upload = () => {
+  return (
+    <div className="Upload">
+    </div>
+  );
+}
+export default Upload
